abab1cceb317ad05872a41444160c56c12b1b37e
Josh Levy <jlevy@glgroup.com>
2015-01-07 21:09:33 -0600

    adding a newline to output

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!\n'
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get('PORT', DEFAULT_PORT)))
